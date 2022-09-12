eec200239ea78225ee93c5f3888a6b66597867e5
Nick-Makuch <nichols.makuch@gmail.com>
2022-09-12 19:22:04 -0400

    Added some comments to LinkedList class

--- src/utilities/LinkedList.java
+++ src/utilities/LinkedList.java
@@ -12,6 +12,9 @@ public class LinkedList<T> {
 	Node _tail;
 	int _size;
 	
+	/**
+	 * Node class
+	 * */
 	private class Node{
 		private T _item;
 		private Node _next;
@@ -22,31 +25,52 @@ public class LinkedList<T> {
 		}
 	}
 	
+	/**
+	 * Constructor: initializes head & tail nodes and size
+	 * */
 	public LinkedList() {
 		this._tail = new Node(null, null);
 		this._head = new Node(null, _tail);
 		this._size = 0;
 	}
 	
+	/**
+	 * checks if List  is empty
+	 * @return boolean
+	 * */
 	public boolean isEmpty() {
 		return this._head._next == _tail;
 	}
 	
+	/**
+	 * Clears list
+	 * */
 	public void clear() {
 		this._head._next = _tail;
 		this._size = 0;
 	}
 	
+	/**
+	 * getter for size
+	 * @return int
+	 * */
 	public int size() {
 		return this._size;
 	}
 	
+	/**
+	 * Adds Node with data "element" to front of list
+	 * */
 	public void addToFront(T element) {
 		Node addedElement = new Node(element, _head._next);
 		this._head._next = addedElement;
 		_size++;
 	}
 	
+	/**
+	 * Checks if Node with data "target" is in the list
+	 * @return boolean
+	 * */
 	public boolean contains (T target) {
 		Node n = _head._next;
 		while(!n.equals(_tail)) {
@@ -56,17 +80,27 @@ public class LinkedList<T> {
 		return false;
 	}
 	
+	/**
+	 * 
+	 * */
 	public Node previous (T target) {
 		if(!this.contains(target)) return null;
 		return previous(target, this._head);
 	}
 	
+	/**
+	 * 
+	 * */
 	private Node previous (T target, Node current) { 
 		if(current.equals(_tail)) return null;
 		if(current._next._item.equals(target)) return current;
 		return previous(target, current._next);
 	}
 	
+	/**
+	 * Removes Node containing data "target"
+	 * @return boolean
+	 * */
 	public boolean remove (T target) {
 		if(this.contains(target)) {
 			// how to get one pointing to this
@@ -76,6 +110,10 @@ public class LinkedList<T> {
 		return false;
 	}
 	
+	/**
+	 * getter for last Node in the list
+	 * @return Node
+	 * */
 	public Node last() {
 		Node current = _head._next;
 		while(!current.equals(_tail)) {
@@ -84,6 +122,9 @@ public class LinkedList<T> {
 		return _head;
 	}
 	
+	/**
+	 * Adds a node with data "element" to back of list
+	 * */
 	public void addToBack(T element) {
 		Node addedElement = new Node(element, _tail);
 		Node last = this.last();
@@ -91,26 +132,44 @@ public class LinkedList<T> {
 		_size++;
 	}
 	
+	/**
+	 * Converts list to a String
+	 * @return String
+	 * */
 	public String toString () {
 		return toStringHelper(new StringBuilder(), _head._next);
 	}
 	
+	/**
+	 * Helper method to convert list to a String
+	 * @return String
+	 * */
 	private String toStringHelper(StringBuilder s, Node n) {
 		if(atEnd(n)) return s.toString();
 		s.append(n._item + " ");
 		return toStringHelper(s, n._next);
 	}
 	
+	/**
+	 * checks to if Node n is the last in the list
+	 * @return boolean
+	 * */
 	public boolean atEnd (Node n) {
 		if (n._next.equals(_tail)) return true;
 		return false;
 	}
 	
+	/**
+	 * 
+	 * */
 	public boolean nextToEnd(Node n) {
 		if (n._next._next.equals(_tail)) return true;
 		return false;
 	}
 	
+	/**
+	 * Reverses that list
+	 * */
 	public void reverse() {
 		
 		LinkedList<T> copyList = this;
@@ -119,6 +178,9 @@ public class LinkedList<T> {
 		this.reverse(copyList, n);
 	}
 	
+	/**
+	 * Helper method to reverse the list recursively 
+	 * */
 	private void reverse(LinkedList<T> list, Node n) {
 		if(n.equals(_head)) return;
 		this.addToBack(n._item);
