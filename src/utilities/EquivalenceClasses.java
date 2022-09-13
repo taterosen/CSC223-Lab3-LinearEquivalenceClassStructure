26b75ce0454d6ddbc3f75e51532646d5bebcdbf5
Nick-Makuch <nichols.makuch@gmail.com>
2022-09-12 20:21:49 -0400

    EquivClasses Skeleton

---
+++ src/utilities/EquivalenceClasses.java
@@ -0,0 +1,45 @@
+package utilities;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class EquivalenceClasses<T>
+{
+	protected Comparator<T> _comparator;
+	protected List<LinkedEquivalenceClass<T>> _rest;
+	
+	public EquivalenceClasses(Comparator<T> c) 
+	{
+		
+	}
+	
+	public boolean add(T element) 
+	{
+		
+	}
+	
+	public boolean contains(T target) 
+	{
+		
+	}
+	
+	public int size() 
+	{
+		
+	}
+	
+	public int numClasses() 
+	{
+		
+	}
+	
+	protected int indexOfClass(T element) 
+	{
+		
+	}
+	
+	public String toString() 
+	{
+		
+	}
+}
