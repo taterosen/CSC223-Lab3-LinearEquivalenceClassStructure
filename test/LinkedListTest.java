2f8c0cc42e1f3a5805ac0240b81f99524e4c2b91
Nick-Makuch <nichols.makuch@gmail.com>
2022-09-13 11:26:37 -0400

    Updata LinkedListTest (toString() doesn't work)
    toString() doesn't work

--- test/LinkedListTest.java
+++ test/LinkedListTest.java
@@ -1,6 +1,9 @@
 
 
 import org.junit.jupiter.api.Test;
+
+import utilities.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest 
@@ -8,6 +11,24 @@ public class LinkedListTest
 	@Test
 	void testToSting() 
 	{
+		LinkedList<Integer> tester = new LinkedList<>();
+		tester.addToFront(1);
+		assertEquals("1", tester.toString());
+		//assertFalse(tester.isEmpty());
+	}
+	
+	@Test
+	void testReverse() 
+	{
 		
+	}
+	
+	@Test
+	void testContains() 
+	{
+		LinkedList<Integer> tester = new LinkedList<>();
+		tester.addToFront(1);
+		assertFalse(tester.isEmpty());
+		assertTrue(tester.contains(1));
 	}
 }
