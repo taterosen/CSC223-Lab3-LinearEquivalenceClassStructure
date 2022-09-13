de0981086397db1d48e2e92044b040cf91c15c58
Nick-Makuch <nichols.makuch@gmail.com>
2022-09-12 20:05:24 -0400

    LinkedEquivClass Skeleton

---
+++ src/utilities/LinkedEquivalenceClass.java
@@ -0,0 +1,77 @@
+package utilities;
+
+import java.util.Comparator;
+
+public class LinkedEquivalenceClass<T> 
+{
+	protected T _canonical;
+	protected Comparator<T> _comparator;
+	protected LinkedList<T> _rest;
+	
+	public LinkedEquivalenceClass(Comparator<T> c)
+	{
+		
+	}
+	
+	public T canonical() 
+	{
+		
+	}
+	
+	public boolean isEmpty() 
+	{
+		
+	}
+	
+	public void clear() 
+	{
+		
+	}
+	
+	public void clearNonCanonical() 
+	{
+		
+	}
+	
+	public int size() 
+	{
+		
+	}
+	
+	public boolean add(T element) 
+	{
+		
+	}
+	
+	public boolean contains(T target) 
+	{
+		
+	}
+	
+	public boolean belongs(T target) 
+	{
+		
+	}
+	
+	public boolean remove(T target) 
+	{
+		
+	}
+	
+	public boolean removeCanonical() 
+	{
+		
+	}
+	
+	public boolean demoteAndSetCanonical(T element) 
+	{
+		
+	}
+	
+	public String toString() 
+	{
+		
+	}
+	
+	
+}
