2521c0c4636d1a566c8517860b0a640defc7d6cd
Nick-Makuch <nichols.makuch@gmail.com>
2022-09-14 15:40:10 -0400

    EquivClassesTest Skeleton

--- test/EquivalenceClassesTest.java
+++ test/EquivalenceClassesTest.java
@@ -5,5 +5,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class EquivalenceClassesTest 
 {
-
+	/**
+	 * 
+	 * */
+	@Test
+	void testAdd() 
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * */
+	@Test
+	void testContains() 
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * */
+	@Test
+	void testSize() 
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * */
+	@Test
+	void testNumClasses() 
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * */
+	@Test
+	void testIndexOfClass() 
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * */
+	@Test
+	void testToString() 
+	{
+		
+	}
 }
