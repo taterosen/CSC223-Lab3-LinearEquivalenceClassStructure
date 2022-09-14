c25ee74156ae036d715c5c7df3587e113a352a18
Nick-Makuch <nichols.makuch@gmail.com>
2022-09-14 15:21:31 -0400

    Update EquivalencyClasses
    Added constructor, size(), and numClasses()

--- src/utilities/EquivalenceClasses.java
+++ src/utilities/EquivalenceClasses.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 import java.util.List;
@@ -14,39 +15,68 @@ import java.util.List;
 
 public class EquivalenceClasses<T>
 {
+	/**
+	 * Instance vars and Constructor for EquivalencyClasses
+	 * */
 	protected Comparator<T> _comparator;
 	protected List<LinkedEquivalenceClass<T>> _rest;
 	
 	public EquivalenceClasses(Comparator<T> c) 
 	{
-		
+		this._comparator = c;
+		this._rest = new ArrayList<>();
 	}
 	
+	/**
+	 * 
+	 * */
 	public boolean add(T element) 
 	{
 		
 	}
 	
+	/**
+	 * 
+	 * */
 	public boolean contains(T target) 
 	{
 		
 	}
 	
+	/**
+	 * Returns total number of elements across all Equiv classes in _rest
+	 * @return int 
+	 * */
 	public int size() 
 	{
-		
+		int size = 0;
+		for(int i = 0; i < numClasses(); i++) 
+		{
+			size += _rest.get(i).size();
+		}
+		return size;
 	}
 	
+	/**
+	 * returns number of Equiv. classes in _rest
+	 * @return int
+	 * */
 	public int numClasses() 
 	{
-		
+		return _rest.size();
 	}
 	
+	/**
+	 * 
+	 * */
 	protected int indexOfClass(T element) 
 	{
 		
 	}
 	
+	/**
+	 * 
+	 * */
 	public String toString() 
 	{
 		
