c324745333b99ef612d44e91b8db8b061c41050a
Miles Dame <milesedame@gmail.com>
2022-09-17 20:22:10 -0400

    Adds isEmpty, clear, and size method tests

--- test/LinkedListTest.java
+++ test/LinkedListTest.java
@@ -6,19 +6,111 @@ import utilities.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LinkedListTest
-{
+public class LinkedListTest {
+	
+	/**
+	 * Tests isEmpty
+	 */
+	@Test
+	void testIsEmpty() {
+		//test on empty list
+		LinkedList<Integer> arr = new LinkedList<Integer>();
+		assertTrue(arr.isEmpty());
+		
+		//test on list with items
+		Integer num1 = 14;
+		Integer num2 = 265;
+		Integer num3 = 420;
+		arr.addToBack(num1);
+		arr.addToBack(num2);
+		arr.addToFront(num3);
+		assertFalse(arr.isEmpty());
+	}
+	
+	/**
+	 * Tests the clear method
+	 */
+	@Test
+	void testClear() {
+		//test on empty list
+		LinkedList<Integer> arr = new LinkedList<Integer>();
+		arr.clear();
+		assertTrue(arr.isEmpty());
+		assertEquals(0, arr.size());
+		
+		//test on list with items
+		Integer num1 = 14;
+		Integer num2 = 265;
+		Integer num3 = 420;
+		arr.addToBack(num1);
+		arr.addToBack(num2);
+		arr.addToFront(num3);
+		arr.clear();
+		assertTrue(arr.isEmpty());
+		assertEquals(0, arr.size());
+	}
+	
+	/**
+	 * Tests the size method
+	 */
+	@Test
+	void testSize() {
+		//test on empty list
+		LinkedList<Integer> arr = new LinkedList<Integer>();
+		arr.clear();
+		assertTrue(arr.isEmpty());
+		assertEquals(0, arr.size());
+		
+		//test on list with items
+		Integer num1 = 14;
+		Integer num2 = 265;
+		Integer num3 = 420;
+		arr.addToBack(num1);
+		arr.addToBack(num2);
+		arr.addToFront(num3);
+		assertEquals(3, arr.size());
+		
+		//test after removing and item
+		arr.remove(num3);
+		assertEquals(2, arr.size());
+	}
+	
 	/**
 	 * Tests addToFront
 	 * */
 	@Test
 	void testAddToFront() {
+		// Test on empty list
 		LinkedList<Integer> arr = new LinkedList<Integer>();
-		assertEquals(0, arr.size());
 		arr.addToFront(1);
 		assertEquals(1, arr.size());
+		assertEquals(1, arr.pop());
 		arr.addToFront(2);
+		assertEquals(2, arr.pop());
 	}
+	
+	/**
+	 * Tests the contains() method using an empty and non-empty Integer LinkedList
+	 * */
+	@Test
+	void testContains()
+	{
+		//Tests on empty list
+		LinkedList<Integer> tester = new LinkedList<>();
+		assertFalse(tester.contains(1));
+
+		//Tests on non-empty list
+		assertTrue(tester.isEmpty());
+		tester.addToFront(1);
+		tester.addToFront(2);
+		tester.addToFront(3);
+		tester.addToFront(4);
+		tester.addToFront(5);
+		assertFalse(tester.isEmpty());
+		assertTrue(tester.contains(1));
+		assertTrue(tester.contains(3));
+	}
+}
 
 	/**
 	 * Tests the toString() method using an empty and non-empty Integer LinkedList
@@ -58,25 +150,4 @@ public class LinkedListTest
 
 	}
 
-	/**
-	 * Tests the contains() method using an empty and non-empty Integer LinkedList
-	 * */
-	@Test
-	void testContains()
-	{
-		//Tests on empty list
-		LinkedList<Integer> tester = new LinkedList<>();
-		assertFalse(tester.contains(1));
-
-		//Tests on non-empty list
-		assertTrue(tester.isEmpty());
-		tester.addToFront(1);
-		tester.addToFront(2);
-		tester.addToFront(3);
-		tester.addToFront(4);
-		tester.addToFront(5);
-		assertFalse(tester.isEmpty());
-		assertTrue(tester.contains(1));
-		assertTrue(tester.contains(3));
-	}
-}
+	
